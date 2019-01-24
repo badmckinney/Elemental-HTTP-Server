c5a399e4c552f1c3c1a449570668b18adc3feb2a
Brad McKinney <brad.mckin@gmail.com>
2019-01-24 10:30:47 -1000

    adds a PUT method to the server

--- server.js
+++ server.js
@@ -1,9 +1,15 @@
 const http = require('http');
 const fs = require('fs');
+const querystring = require('querystring');
 
 const PORT = process.env.PORT || 8080;
 
 const server = http.createServer((req, res) => {
+
+  /*
+  * GET METHOD
+  */
+
   if (req.method === "GET") {
     if (req.url === "/") {
       fs.readFile('./public/index.html', 'utf8', (err, data) => {
@@ -19,7 +25,7 @@ const server = http.createServer((req, res) => {
       if (err) {
         if (err.code === "ENOENT") {
           res.writeHead(404);
-          res.readFile('./public/404.html', 'utf8', (err, data) => {
+          fs.readFile('./public/404.html', 'utf8', (err, data) => {
             if (err) {
               return res.end(`Error: ${err}`);
             } else {
@@ -33,6 +39,11 @@ const server = http.createServer((req, res) => {
         res.end(data);
       }
     });
+
+    /*
+    * POST METHOD
+    */
+
   } else if (req.url === "/elements" && req.method === "POST") {
     let body = "";
 
@@ -41,34 +52,29 @@ const server = http.createServer((req, res) => {
     req.on('end', (chunk) => {
       if (chunk) { body += chunk; }
       else {
-        let bodyData = body.split('&');
-        let elementName = bodyData[0].slice(bodyData[0].indexOf('=') + 1);
-        let elementSymbol = bodyData[1].slice(bodyData[1].indexOf('=') + 1);
-        let elementAtomicNumber = bodyData[2].slice(bodyData[2].indexOf('=') + 1);
-        let description = bodyData[3].slice(bodyData[3].indexOf('=') + 1);
-        let elementDescription = description.split('%20').join(' ').split('%2C').join(' ');
+        let bodyData = querystring.parse(body);
         let newPageTemplate = `
                 <!DOCTYPE html>
         <html lang="en">
 
         <head>
           <meta charset="UTF-8">
-          <title>The Elements - ${elementName}</title>
+          <title>The Elements - ${bodyData.elementName}</title>
           <link rel="stylesheet" href="/css/styles.css">
         </head>
 
         <body>
-          <h1>${elementName}</h1>
-          <h2>${elementSymbol}</h2>
-          <h3>Atomic number ${elementAtomicNumber}</h3>
-          <p>${elementDescription}</p>
+          <h1>${bodyData.elementName}</h1>
+          <h2>${bodyData.elementSymbol}</h2>
+          <h3>Atomic number ${bodyData.elementAtomicNumber}</h3>
+          <p>${bodyData.elementDescription}</p>
           <p><a href="/">back</a></p>
         </body>
 
         </html>
                 `;
 
-        fs.writeFile(`./public/${elementName.toLowerCase()}.html`, newPageTemplate, (err, data) => {
+        fs.writeFile(`./public/${bodyData.elementName.toLowerCase()}.html`, newPageTemplate, (err, data) => {
           if (err) {
             res.writeHead(500);
             return res.end('{ "success": false }');
@@ -88,34 +94,34 @@ const server = http.createServer((req, res) => {
                   fileName = fileName[0].toUpperCase() + fileName.slice(1);
 
                   htmlList += `
-    <li>
-      <a href="${file}">${fileName}</a>
-    </li>
-                `;
+                    <li>
+                      <a href="${file}">${fileName}</a>
+                    </li>
+                  `;
                 }
               });
 
               let newIndexTemplate = `
-<!DOCTYPE html>
-<html lang="en">
+                <!DOCTYPE html>
+                <html lang="en">
 
-<head>
-  <meta charset="UTF-8">
-  <title>The Elements</title>
-  <link rel="stylesheet" href="/css/styles.css">
-</head>
+                <head>
+                  <meta charset="UTF-8">
+                  <title>The Elements</title>
+                  <link rel="stylesheet" href="/css/styles.css">
+                </head>
 
-<body>
-  <h1>The Elements</h1>
-  <h2>These are all the known elements.</h2>
-  <h3>These are ${elementCount}</h3>
-  <ol>
-    ${htmlList}
-  </ol>
-</body>
+                <body>
+                  <h1>The Elements</h1>
+                  <h2>These are all the known elements.</h2>
+                  <h3>These are ${elementCount}</h3>
+                  <ol>
+                    ${htmlList}
+                  </ol>
+                </body>
 
-</html>
-`;
+                </html>
+              `;
 
               fs.writeFile('./public/index.html', newIndexTemplate, (err, data) => {
                 if (err) {
@@ -129,8 +135,68 @@ const server = http.createServer((req, res) => {
         });
       }
     });
+
+    /*
+    * PUT METHOD
+    */
+
+  } else if (req.method === "PUT") {
+
+    fs.stat(`./public/${req.url}`, (err, stats) => {
+      if (err == null) {
+        let body = "";
+
+        req.on('data', (chunk) => { body += chunk; });
+
+        req.on('end', (chunk) => {
+          if (chunk) { body += chunk; }
+          else {
+            let bodyData = querystring.parse(body);
+            let newPageTemplate = `
+              <!DOCTYPE html>
+              <html lang="en">
+      
+              <head>
+                <meta charset="UTF-8">
+                <title>The Elements - ${bodyData.elementName}</title>
+                <link rel="stylesheet" href="/css/styles.css">
+              </head>
+      
+              <body>
+                <h1>${bodyData.elementName}</h1>
+                <h2>${bodyData.elementSymbol}</h2>
+                <h3>Atomic number ${bodyData.elementAtomicNumber}</h3>
+                <p>${bodyData.elementDescription}</p>
+                <p><a href="/">back</a></p>
+              </body>
+      
+              </html>
+            `;
+
+            fs.writeFile(`./public/${bodyData.elementName.toLowerCase()}.html`, newPageTemplate, (err, data) => {
+              if (err) {
+                res.writeHead(500);
+                return res.end('{ "success": false }');
+              }
+
+              res.writeHead(200, { 'Content-Type': 'application/json' });
+              return res.end('{ "success": true }');
+            });
+          }
+        });
+      } else if (err.code === "ENOENT") {
+        res.writeHead(500, { "Content-Type": "application/json" });
+        res.end(`{ "error" : "resource ${req.url} does not exist" }`);
+      } else {
+        res.writeHead(err.status, { "Content-Type": "application/json" });
+        res.end(`{ "error" : ${err.code} }`);
+      }
+    });
+  } else if (req.method === "DELETE") {
+
   }
 });
+
 
 server.listen(PORT, () => {
   console.log(`Running on PORT: ${PORT}`);
